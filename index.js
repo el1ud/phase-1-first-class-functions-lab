876a40d1bcfa64f1b4c70ae59d7a7766ce8e4287
eliud wainaina <wainainaeliud7@gmail.com>
2024-04-11 13:34:35 -0700

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+let drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+function returnFirstTwoDrivers(){
+    return drivers.slice(0,2)
+}
+
+function returnLastTwoDrivers(){
+    return drivers.slice(-2);
+}
+let selectingDrivers = [returnFirstTwoDrivers];
+
+selectingDrivers.push(returnLastTwoDrivers);
+
+function createFareMultiplier(multiplier){
+    return function(value) {
+        return value * multiplier
+    }
+}
+
+function fareDoubler(fare){
+    return fare * 2 ;
+}
+function fareTripler(fare){
+    return fare * 3 ;
+}
+ function selectDifferentDrivers(arrayOfDrivers, selectorFunction){
+    return selectorFunction(arrayOfDrivers) ;
+ }
